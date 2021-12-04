85b5a3c21cc9bff466d21f0eca6f84e811009a08
Sean <sos70305sos@yahoo.com.tw>
2021-12-04 15:46:27 +0800

    Clean TODO Comment

--- app/src/main/java/com/tom/stocktable/network/HttpRequestInterceptor.kt
+++ app/src/main/java/com/tom/stocktable/network/HttpRequestInterceptor.kt
@@ -4,7 +4,7 @@ import android.util.Log
 import okhttp3.Interceptor
 import okhttp3.Response
 
-//TODO 只是記log 攔quest response 中間要做的事情
+//只是記log 攔quest response 中間要做的事情
 class HttpRequestInterceptor : Interceptor {
     val TAG = this.javaClass.name
     override fun intercept(chain: Interceptor.Chain): Response {
